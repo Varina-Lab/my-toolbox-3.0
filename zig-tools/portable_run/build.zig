891531f39ef00c3f0b4c715da06b63e2652973c8
Varina-Lab <varina.lab@proton.me>
2026-04-19 19:17:53 +0700

    Cập nhật

--- zig-tools/portable_run/build.zig
+++ zig-tools/portable_run/build.zig
@@ -1,25 +1,17 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    // Cho phép người dùng chọn target và optimize từ dòng lệnh
-    // Mặc định chúng ta sẽ truyền ReleaseFast từ GitHub Actions
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "portable_run", // Tên file tạm, GH Actions sẽ đổi tên sau
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .name = "portable_run",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
-    // Cấu hình để loại bỏ symbol thừa (strip) nếu là Release
-    if (optimize == .ReleaseFast or optimize == .ReleaseSmall) {
-        exe.root_module.strip = true;
-    }
+    exe.subsystem = .Windows;
 
-    // Cài đặt artifact vào thư mục zig-out/bin
     b.installArtifact(exe);
 }
